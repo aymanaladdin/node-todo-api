aeecc699cddab81fa5b9d08ec968e0fce44d4711
Ayman_Aladdin <ayman.alaa1012@gmail.com>
2017-11-05 16:10:51 +0200

    add DELETE /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,10 +3,12 @@
 const mongoose = require('mongoose');
 
 const DB = "TodoApp";
-const uri = "mongodb://admin:admin@ds245805.mlab.com:45805/todoapp" || `mongodb://localhost:27017/${DB}`;
+const localUri  = `mongodb://localhost:27017/${DB}`;
+
+const mlabUri = "mongodb://admin:admin@ds245805.mlab.com:45805/todoapp" ;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri);
+mongoose.connect(mlabUri);
 
 module.exports = { mongoose };
 
--- server/server.js
+++ server/server.js
@@ -59,6 +59,27 @@ app.get('/todos/:id', (req, res)=>{
 
 });
 
+app.delete('/todos/:id', (req, res)=>{
+
+    let id = req.params.id;
+
+    if(!oid.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then((todo)=>{
+            if(! todo){
+                return res.status(404).send();
+            }
+            res.send({todo})
+        })
+        .catch((err)=>{
+            res.status(400).send();
+        })
+    ;
+});
+
 /*---------------------------------------------------------------------------------------*/
 
 app.listen(port, ()=>{
